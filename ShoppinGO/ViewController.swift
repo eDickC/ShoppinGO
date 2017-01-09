dd43bbae1693c206eba307edf19555a9b4113dce
Eduard Cihuňka <eduard@Eduards-MacBook-Pro.local>
2017-01-09 16:12:23 +0100

    Added ignore for pods, added project

---
+++ ShoppinGO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppinGO
+//
+//  Created by Eduard Cihuňka on 09/01/2017.
+//  Copyright © 2017 Eduard Cihuňka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
